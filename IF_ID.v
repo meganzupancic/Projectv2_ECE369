d71e6ef43e15b25378306fd913d63265c9bf7cdc
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-26 15:16:32 -0700

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -2,14 +2,20 @@
 
 
 module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction_out_IFID,
-            Clk_in); 
+            Clk_in, Rst); 
   
   input [31:0] PCAdder_in_IFID;
   input [31:0] Instruction_in_IFID;
   input Clk_in;
+  input Rst;
 
   output reg [31:0] PCAdder_out_IFID;
   output reg [31:0] Instruction_out_IFID;
+  
+  always @(posedge Rst) begin
+    PCAdder_out_IFID <= 0;
+    Instruction_out_IFID <= 0;
+  end
 
 always @(posedge Clk_in) begin
     PCAdder_out_IFID <= PCAdder_in_IFID;
