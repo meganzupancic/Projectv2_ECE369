3b16b9abcb7efd504e775c1f884950da1b13f49b
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-27 12:38:03 -0700

    Update stage_MEM.v

--- stage_MEM.v
+++ stage_MEM.v
@@ -2,7 +2,7 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
                     ALUAddResult_in_MEM, Zero_MEM, ALUResult_in_MEM, ReadData2_MEM, 
                     mux2_result_in_MEM, MemtoReg_out_MEM, PCSrc_MEM, ALUResult_out_MEM, 
                     mux2_result_out_MEM, ReadData_MEM, RegWrite_out_MEM, ReadData2_out_MEM,
-                    Clk_in, Rst);
+                    size_MEM, Clk_in, Rst);
 
   input MemWrite_MEM;
   input MemRead_MEM;
@@ -14,6 +14,7 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
   input [31:0] ALUResult_in_MEM;
   input [31:0] ReadData2_MEM;
   input [4:0] mux2_result_in_MEM;
+  input [1:0] size_MEM;
   input Clk_in;
   input Rst;
 
@@ -38,8 +39,8 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
   //Branch(Branch, Zero, PCSrc);
   Branch d1(Branch_MEM, Zero_MEM, PCSrc_MEM);
 
-  //DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData)
-  DataMemory d2(ALUResult_in_MEM, ReadData2_MEM, Clk_in, MemWrite_MEM, MemRead_MEM, ReadData_MEM); 
+  //DataMemory(Address, WriteData, Clk, MemWrite, MemRead, size, ReadData)
+  DataMemory d2(ALUResult_in_MEM, ReadData2_MEM, Clk_in, MemWrite_MEM, MemRead_MEM, size_MEM, ReadData_MEM); 
   
 
 endmodule
