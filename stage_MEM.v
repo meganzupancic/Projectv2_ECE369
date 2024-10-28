4059374736d144c1aca1d9294c7ec07f70b19f76
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-27 19:14:41 -0700

    Update stage_MEM.v

--- stage_MEM.v
+++ stage_MEM.v
@@ -1,22 +1,21 @@
 module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWrite_in_MEM, 
-                    ALUAddResult_in_MEM, Zero_MEM, ALUResult_in_MEM, ReadData2_MEM, 
+                    /*ALUAddResult_in_MEM,*/ Zero_MEM, ALUResult_in_MEM, ReadData2_MEM, 
                     mux2_result_in_MEM, MemtoReg_out_MEM, PCSrc_MEM, ALUResult_out_MEM, 
                     mux2_result_out_MEM, ReadData_MEM, RegWrite_out_MEM, ReadData2_out_MEM,
-                    size_MEM, Clk_in, Rst);
+                    size_MEM, Clk_in);
 
   input MemWrite_MEM;
   input MemRead_MEM;
   input Branch_MEM;
   input MemtoReg_in_MEM;
   input RegWrite_in_MEM;
-  input [31:0] ALUAddResult_in_MEM;
+  //input [31:0] ALUAddResult_in_MEM;
   input Zero_MEM;
   input [31:0] ALUResult_in_MEM;
   input [31:0] ReadData2_MEM;
   input [4:0] mux2_result_in_MEM;
   input [1:0] size_MEM;
   input Clk_in;
-  input Rst;
 
   output MemtoReg_out_MEM;
   //output [31:0] ALUAddResult_out_MEM;
@@ -39,7 +38,7 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
   //Branch(Branch, Zero, PCSrc);
   Branch d1(Branch_MEM, Zero_MEM, PCSrc_MEM);
 
-  //DataMemory(Address, WriteData, Clk, MemWrite, MemRead, size, ReadData)
+  //DataMemory(Address, WriteData, Clk_in, MemWrite, MemRead, size, ReadData)
   DataMemory d2(ALUResult_in_MEM, ReadData2_MEM, Clk_in, MemWrite_MEM, MemRead_MEM, size_MEM, ReadData_MEM); 
   
 
