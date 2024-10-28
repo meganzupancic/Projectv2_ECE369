b0668dece0c6aead0ecaf5bee233e18d6c8fac7a
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-27 18:38:54 -0700

    Update MEM_WB.v

--- MEM_WB.v
+++ MEM_WB.v
@@ -9,7 +9,7 @@ module MEM_WB(MemtoReg_in_MEMWB, RegWrite_in_MEMWB, ALUResult_in_MEMWB,
   input MemtoReg_in_MEMWB;
   input RegWrite_in_MEMWB;
   input [31:0]ALUResult_in_MEMWB;
-  input [4:0] ReadData_in_MEMWB;
+  input [31:0] ReadData_in_MEMWB;
   input [4:0] mux2_result_in_MEMWB;
   input Clk_in;
   input Rst;
@@ -17,23 +17,24 @@ module MEM_WB(MemtoReg_in_MEMWB, RegWrite_in_MEMWB, ALUResult_in_MEMWB,
   output reg MemtoReg_out_MEMWB;
   output reg RegWrite_out_MEMWB;
   output reg [31:0]ALUResult_out_MEMWB;
-  output reg [4:0] ReadData_out_MEMWB;
+  output reg [31:0] ReadData_out_MEMWB;
   output reg [4:0] mux2_result_out_MEMWB;
   
-  always @(posedge Rst) begin
-    MemtoReg_out_MEMWB <= 0;
-    RegWrite_out_MEMWB <= 0;
-    ALUResult_out_MEMWB <= 0;
-    ReadData_out_MEMWB <= 0;
-    mux2_result_out_MEMWB <= 0;
-  end
-
-  always @(posedge Clk_in) begin
-    MemtoReg_out_MEMWB <= MemtoReg_in_MEMWB;
-    RegWrite_out_MEMWB <= RegWrite_in_MEMWB;
-    ALUResult_out_MEMWB <= ALUResult_in_MEMWB;
-    ReadData_out_MEMWB <= ReadData_in_MEMWB;
-    mux2_result_out_MEMWB <= mux2_result_in_MEMWB;
+  always @(posedge Rst or posedge Clk_in) begin
+    if (Rst) begin
+        MemtoReg_out_MEMWB <= 0;
+        RegWrite_out_MEMWB <= 0;
+        ALUResult_out_MEMWB <= 0;
+        ReadData_out_MEMWB <= 0;
+        mux2_result_out_MEMWB <= 0;
+    end
+    else if (Clk_in) begin
+        MemtoReg_out_MEMWB <= MemtoReg_in_MEMWB;
+        RegWrite_out_MEMWB <= RegWrite_in_MEMWB;
+        ALUResult_out_MEMWB <= ALUResult_in_MEMWB;
+        ReadData_out_MEMWB <= ReadData_in_MEMWB;
+        mux2_result_out_MEMWB <= mux2_result_in_MEMWB;
+    end
   end
 
 endmodule
