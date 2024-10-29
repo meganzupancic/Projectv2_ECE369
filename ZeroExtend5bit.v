47ec21eed64182458c756aae85bb85bf39ac713b
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-28 22:16:37 -0700

    Update ZeroExtend5bit.v

--- ZeroExtend5bit.v
+++ ZeroExtend5bit.v
@@ -1,1 +1,22 @@
 
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////
+// ECE369 - Computer Architecture
+// 
+// Module - SignExtension.v
+// Description - Sign extension module.
+////////////////////////////////////////////////////////////////////////////////
+module ZeroExtend5Bit(shamt, out);
+
+    /* A 16-Bit input word */
+    //input [15:0] in;
+    input [4:0] shamt;
+    
+    /* A 32-Bit output word */
+    output reg [31:0] out;
+    
+    always @(*) begin
+            out = {27'b0, shamt};
+    end
+    
+endmodule
