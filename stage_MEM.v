7cd45e510bddc506848a6943bf207472f6655c07
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-29 11:58:33 -0700

    Update stage_MEM.v

--- stage_MEM.v
+++ stage_MEM.v
@@ -2,7 +2,7 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
                     /*ALUAddResult_in_MEM,*/ Zero_MEM, ALUResult_in_MEM, ReadData2_MEM, 
                     mux2_result_in_MEM, MemtoReg_out_MEM, PCSrc_MEM, ALUResult_out_MEM, 
                     mux2_result_out_MEM, ReadData_MEM, RegWrite_out_MEM, ReadData2_out_MEM,
-                    size_MEM, Clk_in);
+                    size_MEM, Clk_in, JR_in_MEM, JR_out_MEM);
 
   input MemWrite_MEM;
   input MemRead_MEM;
@@ -16,6 +16,7 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
   input [4:0] mux2_result_in_MEM;
   input [1:0] size_MEM;
   input Clk_in;
+  input JR_in_MEM;
 
   output MemtoReg_out_MEM;
   //output [31:0] ALUAddResult_out_MEM;
@@ -25,12 +26,14 @@ module stage_MEM (MemWrite_MEM, MemRead_MEM, Branch_MEM, MemtoReg_in_MEM, RegWri
   output [31:0] ReadData_MEM;
   output RegWrite_out_MEM;
   output [31:0] ReadData2_out_MEM;
+  output JR_out_MEM;
 
   assign RegWrite_out_MEM = RegWrite_in_MEM;
   assign MemtoReg_out_MEM = MemtoReg_in_MEM;
   assign ALUResult_out_MEM = ALUResult_in_MEM;
   assign mux2_result_out_MEM = mux2_result_in_MEM;
   assign ReadData2_out_MEM = ReadData2_MEM;
+  assign JR_out_MEM = JR_in_MEM;
   //assign ALUAddResult_out_MEM = ALUAddResult_in_MEM;
   
   
