a4361551fd34ae09c7acc9fa0b176d3ea1be41e0
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-29 11:54:14 -0700

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -6,7 +6,7 @@ module EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_
                 MemWrite_out_EXMEM, MemRead_out_EXMEM, Branch_out_EXMEM, MemtoReg_out_EXMEM, 
                 RegWrite_out_EXMEM, ALUAddResult_out_EXMEM, Zero_out_EXMEM, ALUResult_out_EXMEM, 
                 ReadData2_out_EXMEM, mux2_Result_out_EXMEM,
-                size_in_EXMEM, size_out_EXMEM, Clk_in, Rst);
+                size_in_EXMEM, size_out_EXMEM, Clk_in, Rst, JR_in_EXMEM, JR_out_EXMEM);
   
   input MemWrite_in_EXMEM;
   input MemRead_in_EXMEM;
@@ -21,6 +21,7 @@ module EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_
   input [1:0] size_in_EXMEM;
   input Clk_in;
   input Rst;
+  input JR_in_EXMEM;
 
   output reg MemWrite_out_EXMEM;
   output reg MemRead_out_EXMEM;
@@ -33,6 +34,7 @@ module EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_
   output reg [31:0] ReadData2_out_EXMEM;
   output reg [4:0] mux2_Result_out_EXMEM;
   output reg [1:0] size_out_EXMEM;
+  output reg JR_out_EXMEM;
   
   always @(posedge Rst or posedge Clk_in) begin
       if (Rst) begin
@@ -47,6 +49,7 @@ module EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_
           ReadData2_out_EXMEM <= 0;
           mux2_Result_out_EXMEM <= 0;
           size_out_EXMEM <= 0;
+          JR_out_EXMEM <= 0;
       end
       else if (Clk_in) begin
           MemWrite_out_EXMEM <= MemWrite_in_EXMEM;
@@ -60,6 +63,7 @@ module EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_
           ReadData2_out_EXMEM <= ReadData2_in_EXMEM;
           mux2_Result_out_EXMEM <= mux2_Result_in_EXMEM;
           size_out_EXMEM <= size_in_EXMEM;
+          JR_out_EXMEM <= JR_in_EXMEM;
       end
   end
 
